361507542e98775fc482d4d564cc146a7dd26e18
Guljana8 <guljan.amerhanova@gmail.com>
2022-09-29 03:48:20 +0300

    second commit

---
+++ src/finalProject/finalProject1.java
@@ -0,0 +1,15 @@
+package finalProject;
+
+public class finalProject1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is the code for audio");
+		
+		
+		
+
+		
+	}
+
+}
